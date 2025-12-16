71e7d0fd8142d6ca3bcb726943425bbddeddc5e5
Ricardo Delfino <ricardodelfino@proton.me>
2025-12-16 17:37:53 -0300

    feat: complete CS50 Week 7 SQL problem sets
    Includes fiftyville investigation, movies queries, and songs queries

---
+++ week7-sql/movies/12.sql
@@ -0,0 +1,5 @@
+--In 12.sql, write a SQL query to list the titles of all movies in which both Bradley Cooper and Jennifer Lawrence starred.
+--Your query should output a table with a single column for the title of each movie.
+--You may assume that there is only one person in the database with the name Bradley Cooper.
+--You may assume that there is only one person in the database with the name Jennifer Lawrence.
+SELECT title FROM movies WHERE id IN (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Bradley Cooper') AND id IN (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Jennifer Lawrence');
---
+++ week7-sql/movies/13.sql
@@ -0,0 +1,5 @@
+--In 13.sql, write a SQL query to list the names of all people who starred in a movie in which Kevin Bacon also starred.
+--Your query should output a table with a single column for the name of each person.
+--There may be multiple people named Kevin Bacon in the database. Be sure to only select the Kevin Bacon born in 1958.
+--Kevin Bacon himself should not be included in the resulting list.
+SELECT DISTINCT name FROM people JOIN stars ON people.id = stars.person_id WHERE movie_id IN (SELECT movie_id FROM stars JOIN people ON stars.person_id = people.id WHERE name = 'Kevin Bacon' AND birth = 1958) AND name != 'Kevin Bacon';
